60871e3a5c8cdb3412a0a1f45eeee2073c0bfff1
Elizabeth <technology@psiada.org>
2019-10-24 15:55:36 -0400

    Firebase Update

--- app/src/main/java/com/example/psiada3/DelegateResources.java
+++ app/src/main/java/com/example/psiada3/DelegateResources.java
@@ -14,6 +14,76 @@ public class DelegateResources extends AppCompatActivity {
     public Button insta_button;
     public Button contact_button;
     public Button food_button;
+    public Button room_button;
+    public Button social_button;
+    public Button map_button;
+    public Button charity_button;
+    public Button merchandise_button;
+
+    public void merch_nav(){
+        merchandise_button=findViewById(R.id.merch);
+        merchandise_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String merch_url = "https://drive.google.com/open?id=1JaBN_c-FOe4piB3Iz5Hp5pkbu0MVae1P8lHGL4g5sOU";
+                Intent merch_n = new Intent(Intent.ACTION_VIEW);
+                merch_n.setData(Uri.parse(merch_url));
+                startActivity(merch_n);
+            }
+        });
+    }
+
+    public void charity_nav(){
+        charity_button=findViewById(R.id.charity);
+        charity_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String charity_url = "https://drive.google.com/open?id=1JaBN_c-FOe4piB3Iz5Hp5pkbu0MVae1P8lHGL4g5sOU";
+                Intent charity_n = new Intent(Intent.ACTION_VIEW);
+                charity_n.setData(Uri.parse(charity_url));
+                startActivity(charity_n);
+            }
+        });
+    }
+
+    public void social_nav(){
+        social_button=findViewById(R.id.social);
+        social_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String charity_url = "https://drive.google.com/open?id=1JaBN_c-FOe4piB3Iz5Hp5pkbu0MVae1P8lHGL4g5sOU";
+                Intent social_n = new Intent(Intent.ACTION_VIEW);
+                social_n.setData(Uri.parse(charity_url));
+                startActivity(social_n);
+            }
+        });
+    }
+
+    public void map_nav(){
+        map_button=findViewById(R.id.social);
+        map_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String map_url = "https://drive.google.com/open?id=1D6jEOOagStCWYLe2dmbTyeptapaGXriZ";
+                Intent map_n = new Intent(Intent.ACTION_VIEW);
+                map_n.setData(Uri.parse(map_url));
+                startActivity(map_n);
+            }
+        });
+    }
+
+    public void room_nav(){
+        room_button=findViewById(R.id.sched);
+        room_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String room_url = "https://drive.google.com/open?id=1wLH3quwKNDTPo9bTMljFmW2vi5nUElMxyy7_BAfFnQU";
+                Intent room_n = new Intent(Intent.ACTION_VIEW);
+                room_n.setData(Uri.parse(room_url));
+                startActivity(room_n);
+            }
+        });
+    }
 
     public void face_nav(){
         face_button=findViewById(R.id.face);
@@ -73,5 +143,10 @@ public class DelegateResources extends AppCompatActivity {
         insta_nav();
         contact();
         food_nav();
+        room_nav();
+        map_nav();
+        merch_nav();
+        social_nav();
+        charity_nav();
     }
 }
